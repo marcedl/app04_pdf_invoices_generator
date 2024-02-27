c5952b9ccede25423fca296b481719d3044df54c
Marcela DL <leon.ari00@hotmail.com>
2024-02-27 17:15:12 +0100

    adding date from excel file title

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths: 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
+
     invoice = filename.split("-")[0]
+    date = filename.split("-")[1]
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt= f"Invoice nr.{invoice}") 
+    pdf.cell(w=50, h=8, txt= f"Invoice nr.{invoice}", ln=1)
+
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=50, h=8, txt= f"Date {date}", ln=2)  
+    
     pdf.output(f"pdfs/{filename}.pdf")
